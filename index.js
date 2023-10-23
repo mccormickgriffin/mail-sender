3e65b2fe09a8a16cc908ad5b8e92690f02df97bb
Griffin McCormick <mccormickgriffin@gmail.com>
2023-10-23 17:22:45 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const nodemailer = require("nodemailer");
+require("dotenv").config();
+
+const app = express();
+
+// Middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Route
+app.post("/send-email", (req, res) => {
+  const { name, senderEmail, message } = req.body;
+
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: process.env.EMAIL_ADDRESS,
+      pass: process.env.EMAIL_PASSWORD,
+    },
+  });
+
+  const mailOptions = {
+    from: process.env.EMAIL_ADDRESS,
+    to: process.env.EMAIL_ADDRESS,
+    subject: "Message from portfolio site",
+    text: `Name: ${name}\nEmail: ${senderEmail}\nMessage: ${message}`,
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log(error);
+      res.status(500).send("Failed to send email");
+    } else {
+      console.log("Email sent: " + info.response);
+      res.status(200).send("Email sent successfully");
+    }
+  });
+});
+
+// Server
+const appName = process.env.APP_NAME;
+const port = process.env.PORT;
+app.listen(port, () => {
+  console.log(`${appName} is running on port ${port}`);
+});
